7f02b63b3533b6fa1695bc25256a0cb9bcfb287f
doffn <80069784+doffn@users.noreply.github.com>
2024-03-13 00:09:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# importing libraries and packages\
+# importing libraries and packages
 from datetime import datetime
 import telebot
 import threading
@@ -9,12 +9,16 @@ import time
 import os
 import sys
 from tweety import Twitter
-from bot import *
 
 import shutil
 import json
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+
+## importing local functions
+
+from bot import *
+from db import *
 
 API = '''Telegram BOT Token'''
 ID = '''Channal ID'''
@@ -22,12 +26,21 @@ URL = '''Mongodb url'''
 bot = telebot.TeleBot(API)
 
 
-def report(message, channel_id=ID):
 
+def report(message, channel_id=ID):
+    """
+      Sends a message to the specified Telegram channel.
+    
+      Args:
+          message (str): The message content to send.
+          channel_id (str, optional): The Telegram channel ID to send the message to.
+                                       Defaults to the ID stored in the `ID` variable.
+      """
     try:
         bot.send_message(channel_id, message)
     except Exception as e:
         print(f"Failed to send message: {e}")
+
 
 def restart_program():
     """Restarts the current program, with file objects and descriptors
@@ -45,121 +58,7 @@ def restart_program():
 
 
 
-
-def commands():
-    try:
-      data = get_mongo()
-      username = [i for i in data["usernames"] if data["usernames"][i]["Active"]]
-
-    except Exception as e:
-      print(f"Error reading JSON file: {e}")
-    @bot.message_handler(commands=['start'])
-    def handle_start(message):
-        bot.reply_to(message, "Hello! I'm your Telebtot. How can I assist you?")
-
-    @bot.message_handler(commands=['help'])
-    def handle_help(message):
-        bot.reply_to(message, "Here are the available commands:\n"
-                              "/start - Start the bot\n"
-                              "/help - Get help")
-
-    @bot.message_handler(func=lambda message: True)
-    def handle_all_other_messages(message):
-        bot.reply_to(message, "I'm sorry, I don't understand that command. "
-                              "Type /help to see the available commands.")
-
-    @bot.channel_post_handler(commands=['start'])
-    def handle_channel_start(message):
-        bot.reply_to(message, "Hello! I'm Doff bot . I will send new tweet from a useraccount to a telegram.")
-
-    @bot.channel_post_handler(commands=['help'])
-    def handle_channel_help(message):
-        bot.reply_to(message, f"""    Here are the available commands:
-/add <@username> - add a username to the list
-/rem <@username> - remove a username from the list
-/del <@username> - permanently delete a username from the list
-/reset - add the removed usernames back to the working list
-/ls - list all users in the list
-/replies {data['replies']} - include Replies?""")
-
-
-    @bot.channel_post_handler(commands=['add'])
-    def handle_channel_add(message):
-        if message.text.startswith('/add ') and message.text[5:].startswith('@'):
-            if message.text[5:] in username:
-                bot.reply_to(message, f'{message.text[5:]} User is already included...')
-            elif message.text[5:] in data["usernames"]:
-                bot.reply_to(message, f'{message.text[5:]} User is Activated...')
-                data["usernames"][message.text[5:]]["Active"] = True
-                mongo_update(data)
-
-            else:
-                try:
-                    user_name = message.text[6:]
-                    print(user_name)
-                    app = Twitter("session")
-                    app.connect()
-                    id = app.get_user_id(user_name)
-                    data["usernames"][message.text[5:]] = {
-                        "created_date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                        "recent_tweets": [],
-                        "total_tweet" : 0,
-                        "day_tweets" :0,
-                        "Active" : True,
-                        "User_ID" : id
-                    }
-
-                    bot.reply_to(message, f'{message.text[5:]} is added to your account list\n-------╔( •̀ з •́)╝╚(•̀ ▪ •́ )╗-------')
-
-                    mongo_update(data)
-                    restart_program()
-                except Exception as e:
-                    bot.reply_to(message, f'{message.text[5:]} is not a twitter user. Please try again')
-                    print(e)
-
-    @bot.channel_post_handler(commands=['rem'])
-    def handle_channel_rem(message):
-        if message.text.startswith('/rem ') and message.text[5:].startswith('@'):
-            if message.text[5:] not in username:
-                bot.reply_to(message, f'{message.text[5:]} User is not included...')
-            else:
-                data["usernames"][message.text[5:]]["Active"] = False
-                bot.reply_to(message, f'{message.text[5:]} is removed\n-------〵(•ʘ̥ᴗʘ̥ •〵)-------')
-                mongo_update(data)
-                restart_program()
-
-
-    @bot.channel_post_handler(commands=['del'])
-    def handle_channel_rem(message):
-        data = get_mongo()
-        if message.text.startswith('/del ') and message.text[5:].startswith('@'):
-            if message.text[5:] not in username:
-                bot.reply_to(message, f'{message.text[5:]} User is not included...')
-            else:
-                del data["usernames"][message.text[5:]]
-                bot.reply_to(message, f'{message.text[5:]} is Deleteded\n-------〵(•ʘ̥ᴗʘ̥ •〵)-------')
-                mongo_update(data)
-
-                restart_program()
-
-
-    @bot.channel_post_handler(commands=['ls'])
-    def handle_channel_ls(message):
-        if message.text.startswith('/ls'):
-            bot.reply_to(message, f'{username} Total account is: {len(username)}\n╭∩╮(︶0︶)╭∩╮    ╭∩╮(︶0︶)╭∩╮    ╭∩╮(︶0︶)╭∩╮')
-
-    @bot.channel_post_handler(commands=['replies'])
-    def handle_channel_replies(message):
-        replies = data["replies"]
-        data["replies"] = not replies
-        bot.reply_to(message, f'THE REPLIES HAVE BEEN CHANGED : {data["replies"]}')
-        mongo_update(data)
-
-
-    bot.polling()
-
-
-session = ["session", "session1"]
+session = ["session", "session1"]   # define multiple account session to avoid rate limitation
 
 
 def get_values(values):
@@ -287,7 +186,7 @@ def main_function():
       else:
           print("You are sleeping")
       print("FINISHED")
-      time.sleep(5*60)
+      time.sleep(5*60)   # it will sleep for 5 minute
 
 
 
@@ -318,7 +217,7 @@ def reviewer():
         print(e)
 
 def review():
-    # Schedule the function to run at 12 PM
+    """ Schedule the function to run at 12 PM"""
     schedule.every().day.at("12:00",).do(reviewer)
 
     # Keep the schedule running continuously
